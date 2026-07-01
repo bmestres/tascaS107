ee1b72e722fa67a7786b34a8a82968995fcdc469
Bernat Mestres <ber.mestres@gmail.com>
2026-07-01 16:27:08 +0200

    feat: created SerializationToJASON class to manage serialization and deserialization of given objects to JSON

--- src/main/java/nivell02/exercice01/SerializationToJSON.java
+++ src/main/java/nivell02/exercice01/SerializationToJSON.java
@@ -1,9 +1,23 @@
 package nivell02.exercice01;
 
 
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
 public class SerializationToJSON {
 
-    String filePath = "/Users/bernat/IdeaProjects/tascaS107";
+    private final String filePath;
+    private Object o;
 
+    public SerializationToJSON(String filePath, Object o){
+        this.filePath = filePath;
+        this.o = o;
+    }
 
+    public void serialize(){
+        File file = new File(this.filePath);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(file, this.o);
+    }
 }
